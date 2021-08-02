fbf1d932f096b2bdb2715258f47ce4cfee273cc5
Nikolas Burkoff <nikolas.burkoff@tessella.com>
2021-08-02 14:42:42 +0100

    Replace topological sort with version from teal

--- R/graph_methods.R
+++ R/graph_methods.R
@@ -1,51 +1,65 @@
-#' Topologically sorts nodes so that parents are listed before all their children
 #'
-#' @param child_to_parents  mapping from child to its parents (upstream dependencies)
+#' Topological graph sort
 #'
+#' Graph is a list which for each node contains a vector of child nodes
+#' in the returned list, parents appear before their children.
+#'
+#' Implementation of Kahn algorithm with a modification to maintain the order of input elements.
+#'
+#' @param graph (named `list`) list with node vector elements mapping from
+#'   child to its parents (upstream dependencies)
 #' @return vector listing parents before children
-#'
 #' @examples
-#' topological_sort <- staged.dependencies:::topological_sort
-#'
-#' all(topological_sort(list(
-#' n1 = c(), n2 = c("n1"), n3 = c("n2"),
-#' n4 = c("n2", "n3"))) == c("n1", "n2", "n3", "n4")
-#' )
-#' is.null(topological_sort(list()))
-#'
+#'   staged.dependencies:::topological_sort(list(A = c(), B = c("A"), C = c("B"), D = c("A")))
+#'   staged.dependencies:::topological_sort(list(D = c("A"), A = c(), B = c("A"), C = c("B")))
+#'   staged.dependencies:::topological_sort(list(D = c("A"), B = c("A"), C = c("B"), A = c()))
 #' \dontrun{
-#' # cycle
-#' topological_sort(list(
-#'   n1 = c("n2"), n2 = c("n3", "n1"), n3 = c()
-#' ))
+#'   # cycle
+#'   topological_sort(list(A = c("B"), B = c("C", "A"), C = c()))
 #' }
-topological_sort <- function(child_to_parents) {
-  # depth-first search from children to parents, then output nodes in the order of finishing times
-  ordering <- c()
-  visiting <- list()
-
-  treat_node <- function(node) {
-    # cat_nl("Treating node '", node, "'")
-    # Sys.sleep(1)
-    # print(visiting)
-    if (node %in% names(visiting)) {
-      stop("Node ", node, " forms part of a cycle.")
+topological_sort <- function(graph) {
+  # compute in-degrees
+  in_degrees <- list()
+  for (node in names(graph)) {
+    in_degrees[[node]] <- 0
+    for (to_edge in graph[[node]]) {
+      in_degrees[[to_edge]] <- 0
     }
-    visiting[[node]] <<- TRUE
-    for (parent in setdiff(child_to_parents[[node]], ordering)) {
-      treat_node(parent)
-    }
-    visiting[[node]] <<- NULL # remove
-    ordering <<- c(ordering, node)
   }
 
-  nodes_to_process <- names(child_to_parents)
-  while (length(nodes_to_process) > 0) {
-    treat_node(nodes_to_process[[1]])
-    nodes_to_process <- setdiff(nodes_to_process, ordering)
+  for (node in graph) {
+    for (to_edge in node) {
+      in_degrees[[to_edge]] <- in_degrees[[to_edge]] + 1
+    }
   }
 
-  ordering
+  # sort
+  visited <- 0
+  sorted <- list()
+  zero_in <- list()
+  for (node in names(in_degrees)) {
+    if (in_degrees[[node]] == 0) zero_in <- append(zero_in, node)
+  }
+  zero_in <- rev(zero_in)
+
+  while (length(zero_in) != 0) {
+    visited <- visited + 1
+    sorted <- c(zero_in[[1]], sorted)
+    for (edge_to in graph[[zero_in[[1]]]]) {
+      in_degrees[[edge_to]] <- in_degrees[[edge_to]] - 1
+      if (in_degrees[[edge_to]] == 0) {
+        zero_in <- append(zero_in, edge_to, 1)
+      }
+    }
+    zero_in[[1]] <- NULL
+  }
+
+  if (visited != length(in_degrees)) {
+    stop("Graph is not a directed acyclic graph. Cycles involving nodes: ",
+         paste0(setdiff(names(in_degrees), sorted), collapse = " "))
+  } else {
+    return(unlist(sorted))
+  }
 }
 
 # get the descendants (all children) of node, given list mapping parent to children
